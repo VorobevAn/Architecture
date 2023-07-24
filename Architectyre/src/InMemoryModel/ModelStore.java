c0075ddf15fb8d4a0c790c7dd3d0b3a4fa2b68d7
Andrey <cust89@mail.ru>
2023-07-24 10:22:22 +0500

    Initial commit

---
+++ Architectyre/src/InMemoryModel/ModelStore.java
@@ -0,0 +1,12 @@
+package InMemoryModel;
+
+import java.util.List;
+
+public class ModelStore {
+    public List<PoligonModel> models;
+    public List<Scene> scenes;
+    public List<Flash> flashes;
+    public List<Camera> cameras;
+    private List<iModelChangeObserver> changeObservers;
+архитектура
+}
